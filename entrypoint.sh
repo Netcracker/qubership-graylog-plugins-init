a757adfe2e41032ab7e49df3fa8249cf303dcf43
IldarMinaev <ildar.minaev.qubership@gmail.com>
2026-08-21 14:54:27 +0400

    ci: add container runtime gate

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ GRAYLOG_PLUGINS_DIR=/usr/share/graylog/plugin
 echo "Start graylog-plugins-init container..."
 
 echo "Available plugins:"
-ls -lah ${LOCAL_PLUGINS_DIR}
+ls -lah "${LOCAL_PLUGINS_DIR}"
 
 echo "Copy plugins from ${LOCAL_PLUGINS_DIR} to ${GRAYLOG_PLUGINS_DIR} (which should be mounted as a volume)..."
 mkdir -p "${GRAYLOG_PLUGINS_DIR}"
